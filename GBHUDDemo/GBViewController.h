aaac81601c127bcb1d4de7db6cfc74c8c8592477
Luka Mirosevic <luka@goonbee.com>
2012-11-21 14:25:30 +0100

    Initial Commit

---
+++ GBHUDDemo/GBViewController.h
@@ -0,0 +1,13 @@
+//
+//  GBViewController.h
+//  GBHUDDemo
+//
+//  Created by Luka Mirosevic on 21/11/2012.
+//  Copyright (c) 2012 Goonbee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GBViewController : UIViewController
+
+@end
---
+++ GBHUDDemo/GBViewController.m
@@ -0,0 +1,29 @@
+//
+//  GBViewController.m
+//  GBHUDDemo
+//
+//  Created by Luka Mirosevic on 21/11/2012.
+//  Copyright (c) 2012 Goonbee. All rights reserved.
+//
+
+#import "GBViewController.h"
+
+@interface GBViewController ()
+
+@end
+
+@implementation GBViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
